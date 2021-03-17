4ff0dd421ab27a16a570cc39c981ae67a6aabe15
Tory Moscaritolo <torymoscaritolo@gmail.com>
2021-03-17 13:20:58 +1100

    fix line breaks

---
+++ index.js
@@ -0,0 +1,5 @@
+const navAbout = document.getElementById('nav-about');
+const navContact = document.getElementById('nav-contact');
+const navProjects = document.getElementById('nav-projects');
+
+//Create a scroll speed when clicking on the navbar links
