01c91f9850909efdd4d507d81580c65ae53663b8
Tory Moscaritolo <torymoscaritolo@gmail.com>
2021-03-22 16:15:24 +1100

    fix footer

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ const navContact = document.getElementById('nav-contact');
 const navProjects = document.getElementById('nav-projects');
 
 //Create a scroll speed when clicking on the navbar links
+
+
+//Spotify SDK
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const token = '[My Spotify Web API access token]';
+    const player = new Spotify.Player({
+      name: 'Web Playback SDK Quick Start Player',
+      getOAuthToken: cb => { cb(token); }
+    });
+  
+    // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+  
+    // Playback status updates
+    player.addListener('player_state_changed', state => { console.log(state); });
+  
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+    });
+  
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+  
+    // Connect to the player!
+    player.connect();
+  };
