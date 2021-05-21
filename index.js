0d9607afe2736792d7339151ce5f548fb3d3f00f
Tory Moscaritolo <torymoscaritolo@gmail.com>
2021-05-21 11:56:04 +1000

    update html and remove js file

--- index.js
+++
@@ -1,5 +0,0 @@
-const navAbout = document.getElementById('nav-about');
-const navContact = document.getElementById('nav-contact');
-const navProjects = document.getElementById('nav-projects');
-
-//Create a scroll speed when clicking on the navbar links
